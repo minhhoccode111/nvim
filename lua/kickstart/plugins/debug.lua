7e921dadb27d230d935ec6b4a3c5ebaecf3be21c
minhhoccode111 <minhhoccode111@gmail.com>
2024-08-17 19:26:19 +0700

    add: config debugger

--- lua/kickstart/plugins/debug.lua
+++ lua/kickstart/plugins/debug.lua
@@ -52,6 +52,7 @@ return {
     vim.keymap.set('n', '<F1>', dap.step_into, { desc = 'Debug: Step Into' })
     vim.keymap.set('n', '<F2>', dap.step_over, { desc = 'Debug: Step Over' })
     vim.keymap.set('n', '<F3>', dap.step_out, { desc = 'Debug: Step Out' })
+    vim.keymap.set('n', '<F4>', dap.step_out, { desc = 'Debug: Step Back' })
     vim.keymap.set('n', '<leader>Db', dap.toggle_breakpoint, { desc = '[D]ebug: [B]reakpoint Toggle' })
     vim.keymap.set('n', '<leader>DB', function()
       dap.set_breakpoint(vim.fn.input 'Breakpoint condition: ')
@@ -63,19 +64,87 @@ return {
       -- Set icons to characters that are more likely to work in every terminal.
       --    Feel free to remove or use ones that you like more! :)
       --    Don't feel like these are good choices.
-      icons = { expanded = '▾', collapsed = '▸', current_frame = '*' },
       controls = {
+        element = 'repl',
+        enabled = true,
         icons = {
-          pause = '⏸',
-          play = '▶',
-          step_into = '⏎',
-          step_over = '⏭',
-          step_out = '⏮',
-          step_back = 'b',
-          run_last = '▶▶',
-          terminate = '⏹',
-          disconnect = '⏏',
+          disconnect = '',
+          pause = '',
+          play = '',
+          run_last = '',
+          step_back = '',
+          step_into = '',
+          step_out = '',
+          step_over = '',
+          terminate = '',
         },
+      },
+      element_mappings = {},
+      expand_lines = true,
+      floating = {
+        border = 'single',
+        mappings = {
+          close = { 'q', '<Esc>' },
+        },
+      },
+      force_buffers = true,
+      icons = {
+        collapsed = '',
+        current_frame = '',
+        expanded = '',
+      },
+      layouts = {
+        {
+          elements = {
+            {
+              id = 'scopes',
+              -- size = 0.25,
+              size = 1.0,
+            },
+            -- {
+            --   id = 'breakpoints',
+            --   -- size = 0.25,
+            --   size = 0.05,
+            -- },
+            -- {
+            --   id = 'stacks',
+            --   -- size = 0.25,
+            --   size = 0.05,
+            -- },
+            -- {
+            --   id = 'watches',
+            --   -- size = 0.25,
+            --   size = 0.05,
+            -- },
+          },
+          position = 'left',
+          -- size = 40,
+          size = 0.5,
+        },
+        {
+          elements = { {
+            id = 'repl',
+            size = 0.5,
+          }, {
+            id = 'console',
+            size = 0.5,
+          } },
+          position = 'bottom',
+          -- size = 10,
+          size = 0.1,
+        },
+      },
+      mappings = {
+        edit = 'e',
+        expand = { '<CR>', '<2-LeftMouse>' },
+        open = 'o',
+        remove = 'd',
+        repl = 'r',
+        toggle = 't',
+      },
+      render = {
+        indent = 1,
+        max_value_lines = 100,
       },
     }
 
