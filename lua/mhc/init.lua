103a6384087b75956e583ac06202f92f6d2238b8
minhhoccode111 <minhhoccode111@gmail.com>
2024-09-18 21:41:58 +0700

    refactor

---
+++ lua/mhc/init.lua
@@ -0,0 +1,28 @@
+require 'mhc.set'
+require 'mhc.map'
+require 'mhc.ui'
+return {
+  require 'mhc.plugins.autopairs',
+  require 'mhc.plugins.autotag',
+  require 'mhc.plugins.bufferline',
+  require 'mhc.plugins.colorschemes',
+  require 'mhc.plugins.comment',
+  require 'mhc.plugins.conform',
+  require 'mhc.plugins.context',
+  require 'mhc.plugins.debug',
+  require 'mhc.plugins.gitsigns',
+  require 'mhc.plugins.indent_line',
+  require 'mhc.plugins.lint',
+  require 'mhc.plugins.lspconfig',
+  require 'mhc.plugins.markdownpreview',
+  require 'mhc.plugins.mini',
+  require 'mhc.plugins.neotree',
+  require 'mhc.plugins.nvimcmp',
+  require 'mhc.plugins.other',
+  require 'mhc.plugins.rest',
+  require 'mhc.plugins.tailwind',
+  require 'mhc.plugins.telescope',
+  require 'mhc.plugins.termimal',
+  require 'mhc.plugins.treesitter',
+  require 'mhc.plugins.whichkey',
+}
