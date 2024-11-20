bc32ba1301ddec3d2c6b55adf90a9207d42bd4da
Hoang Minh <minhhoccode111@gmail.com>
2024-11-20 16:55:48 +0700

    add: noice pop up window size

--- lua/mhc/plugins/noice.lua
+++ lua/mhc/plugins/noice.lua
@@ -6,6 +6,34 @@ return {
     require('noice').setup {
       presets = { inc_rename = true },
     }
+
+    -- -- calculate the dynamic window size
+    -- local function calculate_size()
+    --   local ui = vim.api.nvim_list_uis()[1] -- Get current Neovim UI dimensions
+    --   local aspect_ratio = 0.5 -- Define your desired aspect ratio
+    --   local width = math.floor(ui.width * aspect_ratio)
+    --   local height = math.floor(ui.height * aspect_ratio)
+    --   return { width = width, height = height }
+    -- end
+
+    -- require('noice').setup {
+    --   views = {
+    --     popup = {
+    --       size = calculate_size(), -- Use calculated size dynamically
+    --     },
+    --   },
+    -- }
+
+    -- require('notify').setup {
+    --   render = 'compact',
+    --   on_open = function(win)
+    --     local size = calculate_size()
+    --     vim.api.nvim_win_set_config(win, {
+    --       width = size.width,
+    --       height = size.height,
+    --     })
+    --   end,
+    -- }
   end,
   dependencies = {
     -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
@@ -13,6 +41,8 @@ return {
     -- OPTIONAL:
     --   `nvim-notify` is only needed, if you want to use the notification view.
     --   If not available, we use `mini` as the fallback
-    'rcarriga/nvim-notify',
+    { 'rcarriga/nvim-notify', opts = {
+      render = 'compact',
+    } },
   },
 }
