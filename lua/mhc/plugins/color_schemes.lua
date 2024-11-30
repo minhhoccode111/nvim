e0fcc44359a76b7d951335668d251110b63bb565
minhhoccode111 <minhhoccode111@gmail.com>
2024-11-30 23:11:56 +0700

    default vscode

--- lua/mhc/plugins/color_schemes.lua
+++ lua/mhc/plugins/color_schemes.lua
@@ -1,4 +1,4 @@
--- themes
+-- colorschemes
 
 return {
   { -- You can easily change to a different colorscheme.
@@ -13,7 +13,8 @@ return {
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
       -- vim.cmd.colorscheme 'randomhue'
-      vim.cmd.colorscheme 'catppuccin-macchiato'
+      -- vim.cmd.colorscheme 'catppuccin-macchiato'
+      vim.cmd.colorscheme 'vscode'
 
       -- You can configure highlights by doing something like:
       vim.cmd.hi 'Comment gui=none'
