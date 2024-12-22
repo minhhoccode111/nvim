8d595f34c6980311dbf136f0ef111f7efc396207
minhhoccode111 <minhhoccode111@gmail.com>
2024-12-22 17:26:54 +0700

    add: dynamic local neovim color scheme

--- lua/mhc/plugins/themes.lua
+++ lua/mhc/plugins/themes.lua
@@ -1,10 +1,10 @@
 -- colorschemes
 
 return {
-  { -- You can easily change to a different colorscheme.
+  {
+    -- You can easily change to a different colorscheme.
     -- Change the name of the colorscheme plugin below, and then
     -- change the command in the config to whatever the name of that colorscheme is.
-    --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
     'folke/tokyonight.nvim',
     priority = 1000, -- Make sure to load this before all the other start plugins.
@@ -12,10 +12,13 @@ return {
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      -- vim.cmd.colorscheme 'randomhue'
-      -- vim.cmd.colorscheme 'catppuccin-macchiato'
-      -- vim.cmd.colorscheme 'catppuccin-mocha'
-      vim.cmd.colorscheme 'gruvbox-material'
+      -- vim.cmd.colorscheme 'tokyonight'
+
+      -- Load the colorscheme from the environment variable or default to a fallback
+      -- this $NVIM_THEME will be set in ~/.extra because I don't want to commit
+      -- every single time I change my theme
+      local theme = vim.env.NVIM_THEME or 'tokyonight'
+      pcall(vim.cmd.colorscheme, theme)
 
       -- You can configure highlights by doing something like:
       vim.cmd.hi 'Comment gui=none'
