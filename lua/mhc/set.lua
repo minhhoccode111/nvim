59a1702a7a02a166aa0103bd5b6c1e5f823fffa4
minhhoccode111 <minhhoccode111@gmail.com>
2025-01-02 09:38:12 +0700

    add: transparency

--- lua/mhc/set.lua
+++ lua/mhc/set.lua
@@ -115,25 +115,25 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
--- -- make neovim inherit the transparent background of terminal
--- vim.api.nvim_create_autocmd('ColorScheme', {
---   callback = function()
---     local highlights = {
---       'Normal',
---       'LineNr',
---       'Folded',
---       'NonText',
---       'SpecialKey',
---       'VertSplit',
---       'SignColumn',
---       'EndOfBuffer',
---       'TablineFill', -- this is specific to how I like my tabline to look like
---     }
---     for _, name in pairs(highlights) do
---       vim.cmd.highlight(name .. ' guibg=none ctermbg=none')
---     end
---   end,
--- })
+-- make neovim inherit the transparent background of terminal
+vim.api.nvim_create_autocmd('ColorScheme', {
+  callback = function()
+    local highlights = {
+      'Normal',
+      'LineNr',
+      'Folded',
+      'NonText',
+      'SpecialKey',
+      'VertSplit',
+      'SignColumn',
+      'EndOfBuffer',
+      'TablineFill', -- this is specific to how I like my tabline to look like
+    }
+    for _, name in pairs(highlights) do
+      vim.cmd.highlight(name .. ' guibg=none ctermbg=none')
+    end
+  end,
+})
 
 -- disable some default providers
 vim.g['loaded_node_provider'] = 0
