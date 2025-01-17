e002265c2e52bd3b5405f643aa30ceee1c4ba851
minhhoccode111 <minhhoccode111@gmail.com>
2025-01-17 19:41:42 +0700

    add: relative line number when toggle neotree

--- lua/mhc/plugins/neotree.lua
+++ lua/mhc/plugins/neotree.lua
@@ -12,15 +12,27 @@ return {
   },
   config = function()
     require('neo-tree').setup {
+      event_handlers = {
+        {
+          event = 'neo_tree_buffer_enter',
+          handler = function(arg)
+            vim.cmd [[
+          setlocal relativenumber
+        ]]
+          end,
+        },
+      },
       filesystem = {
         filtered_items = {
           -- hide_dotfiles = false,
           -- hide_gitignored = false,
           never_show = {
             '.git',
+            'node_modules',
           },
           always_show = { -- remains visible even if other settings would normally hide it
             '.gitignore',
+            '.github',
             '.env',
             '.env.test',
             '.env.production',
