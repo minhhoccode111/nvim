b975a12d2fce2deb7bd892f3673ffdc1dc9dd606
minhhoccode111 <minhhoccode111@gmail.com>
2025-04-12 21:31:52 +0700

    update: flutter-tools.nvim
    .

--- README.md
+++ README.md
@@ -1,23 +1,22 @@
-# My Nvim Config
+# minhhoccode's nvim config
 
 **Warning**: If you want to give these config a try, you should first fork this
 repository, review the code, and remove things you don’t want or need. Don’t
 blindly use my settings unless you know what that entails. Use at your own
 risk!
 
-This is a fork of [kickstart.nvim](https://github.com/nvim-lua/kickstart.nvim)
-which is a project to help me get started on my neovim journey.
+This is a fork of [kickstart.nvim](https://github.com/nvim-lua/kickstart.nvim), a project to help me get started on my Neovim journey.
 
 ## Getting started
 
-Learn the basics if you're new
+Learn the basics
 
-[From VSCode to Nvim (Four Easy
-Steps)](https://gist.github.com/minhhoccode111/0833779e96e37d0b05c1303ee4466770)
+[From VSCode to NVim (Four Easy Steps)](https://minhhoccode111.github.io/blog/from-vscode-to-nvim/)
 
 Clone the repository
 
 ```bash
+# Have to place it at ~/.config/nvim/
 git clone git@github.com:minhhoccode111/nvim.git ~/.config/nvim
 ```
 
--- lua/mhc/plugins/flutter.lua
+++ lua/mhc/plugins/flutter.lua
@@ -1,23 +1,23 @@
 return {
-  {
-    'nvim-flutter/flutter-tools.nvim',
-    lazy = false,
-    dependencies = {
-      'nvim-lua/plenary.nvim',
-      'stevearc/dressing.nvim', -- optional for vim.ui.select
-    },
-    config = function()
-      require('flutter-tools').setup {} -- use default
-
-      local map = vim.keymap.set
-
-      map('n', '<leader>fF', function()
-        require('telescope').extensions.flutter.commands()
-      end, { desc = '[F]ind [F]lutter Command to Run' })
-
-      map('n', '<leader>;', function()
-        require('telescope').extensions.flutter.commands()
-      end, { desc = '[F]ind [F]lutter Command to Run' })
-    end,
+  'nvim-flutter/flutter-tools.nvim',
+  lazy = false,
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'stevearc/dressing.nvim', -- optional for vim.ui.select
   },
+  config = function()
+    require('flutter-tools').setup {
+      flutter_path = '/home/mhc/fvm/default/bin/flutter',
+    }
+
+    local map = vim.keymap.set
+
+    map('n', '<leader>fF', function()
+      require('telescope').extensions.flutter.commands()
+    end, { desc = '[F]ind [F]lutter Command to Run' })
+
+    map('n', '<leader>;', function()
+      require('telescope').extensions.flutter.commands()
+    end, { desc = '[F]ind [F]lutter Command to Run' })
+  end,
 }
