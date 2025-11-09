b646cda5b01783d5bfa5b52d2d4314dcb1f5e1c4
minhhoccode111 <minhhoccode111@gmail.com>
2025-11-09 11:32:55 +0700

    add: image preview for ghostty terminal using snacks.nvim

--- lua/mhc/plugins/snacks.lua
+++ lua/mhc/plugins/snacks.lua
@@ -8,5 +8,24 @@ return {
     bigfile = { enabled = true },
     -- indent = { enabled = true },
     quickfile = { enabled = true },
+    image = {
+      enabled = true,
+      doc = {
+        enabled = true,
+        inline = false,
+        float = false,
+        max_width = 10,
+        max_height = 10,
+      },
+    },
+  },
+  keys = {
+    {
+      '<leader>i',
+      function()
+        require('snacks').image.hover()
+      end,
+      desc = '[I]mage Preview',
+    },
   },
 }
