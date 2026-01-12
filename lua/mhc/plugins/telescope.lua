caabbf370a2111b1d5319627097905d6d3b7842f
minhhoccode111 <minhhoccode111@gmail.com>
2026-01-12 18:15:54 +0700

    fix: replace live_grep with grep_string in telescope

--- lua/mhc/plugins/telescope.lua
+++ lua/mhc/plugins/telescope.lua
@@ -136,7 +136,11 @@ return { -- Fuzzy Finder (files, lsp, etc)
     end, { desc = '[F]ind [F]iles' })
     map('n', '<leader>fS', builtin.builtin, { desc = '[F]ind [S]elect Telescope' })
     map('n', '<leader>fw', builtin.grep_string, { desc = '[F]ind Current [W]ord' })
-    map('n', '<leader>fg', builtin.live_grep, { desc = '[F]ind [G]rep String' })
+    map('n', '<leader>fg', function()
+      builtin.grep_string {
+        search = vim.fn.input 'rg > ',
+      }
+    end, { desc = '[F]ind [G]rep String' })
     map('n', '<leader>fd', builtin.diagnostics, { desc = '[F]ind [D]iagnostics' })
     map('n', '<leader>fo', builtin.oldfiles, { desc = '[F]ind [O]ld (Opened) Files' })
     map('n', '<leader>fb', builtin.buffers, { desc = '[F]ind Current [B]uffers' })
