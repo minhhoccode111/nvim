2489b625d025c61500e7a0b1418f8e830c7534a5
minhhoccode111 <minhhoccode111@gmail.com>
2026-09-12 16:11:03 +0700

    fix: replace deprecated vim.hl.on_yank with vim.hl.hl_op

--- lua/mhc/set.lua
+++ lua/mhc/set.lua
@@ -109,12 +109,12 @@ vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
 -- Highlight when yanking (copying) text
 --  Try it with `yap` in normal mode
---  See `:help vim.highlight.on_yank()`
+--  See `:help vim.hl.hl_op()`
 vim.api.nvim_create_autocmd('TextYankPost', {
   desc = 'Highlight when yanking (copying) text',
   group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
   callback = function()
-    vim.hl.on_yank()
+    vim.hl.hl_op()
   end,
 })
 
